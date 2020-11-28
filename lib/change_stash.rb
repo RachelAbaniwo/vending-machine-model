9316e5d707c40e8163ac566177ec1f1086c09afe
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2020-11-28 08:34:44 +0100

    - add code to preload and restock change stage - include tests

--- lib/change_stash.rb
+++ lib/change_stash.rb
@@ -12,4 +12,32 @@ module ChangeStash
       "1p" => 0,
     }
   end
+
+  def load_change_stash change_amount=nil
+    # initial load
+    unless change_amount
+      @change_stash.each do |change_amount_key, change_amount_quantity|
+        update_change_stash(change_amount_key)
+      end
+    else
+      # change amount exists when it is a reload(restock)
+      update_change_stash(change_amount)
+    end
+  end
+
+  def restock_change_stash
+    puts "Select a change amount you want to restock by entering its change amount"
+    change_amount = gets.chomp
+    load_change_stash(change_amount)
+  end
+
+  def update_change_stash change_amount
+    puts "Enter the quantity of this denomination of change you want to load"
+    quantity = gets.chomp.to_i
+    @change_stash[change_amount] += quantity
+  end
+
+  def update_stash_after_change change_denomination
+    @change_stash[change_denomination] -= 1 if change_denomination
+  end
 end
--- spec/change_stash_spec.rb
+++ spec/change_stash_spec.rb
@@ -17,6 +17,31 @@ describe 'change_stash' do
     it 'when initialized, each change entry should have a quantity of zero' do
       expect(@test_change_stash.change_stash["2p"]).to eq 0
     end
+  end
+
+  describe '#load_change_stash' do
+    it 'when called and input is 10, each product item should have a quantity of 10 after initial loading' do
       
+      expect(@test_change_stash.change_stash["£2"]).to eq 0
+
+      allow_any_instance_of(Object).to receive(:gets).and_return "10"
+      @test_change_stash.load_change_stash
+
+      expect(@test_change_stash.change_stash["£2"]).to eq 10
+      expect(@test_change_stash.change_stash["20p"]).to eq 10
+      expect(@test_change_stash.change_stash["2p"]).to eq 10
+    end
+  end
+
+  describe '#restock_change_stash' do
+    it 'when called and inputs are £2 and 14, £2 should have a quantity update from 0 to 14 after restock' do
+
+      expect(@test_change_stash.change_stash["£2"]).to eq 0
+
+      allow_any_instance_of(Object).to receive(:gets).and_return("£2", "14")
+      @test_change_stash.restock_change_stash
+
+      expect(@test_change_stash.change_stash["£2"]).to eq 14
+    end
   end
 end
